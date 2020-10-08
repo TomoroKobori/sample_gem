3e616e37e12dd0ca9c2c2c176edceed4355c3416
kobori <46224993+TomoroKobori@users.noreply.github.com>
2020-10-08 22:19:33 +0900

    テストを追加

--- spec/sample_gem_spec.rb
+++ spec/sample_gem_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe SampleGem do
     expect(SampleGem::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it ".hello" do
+    expect(SampleGem.hello).to eq("Hello")
   end
 end
